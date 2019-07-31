f75e4af9b300465da2860c30ddfdcd89a09e93ed
Marco Stroppa <marco@ircam.fr>
2019-07-31 16:15:58 +0200

    initial import (2)

---
+++ marco-sources/vps/get-xp.lisp
@@ -0,0 +1,1 @@
+(in-package cr)(defun get-xp (wanted-fq actual-fq)"return the transposition interval in midicents to achieve the wanted frequency from the actual frequency"  (round   (* (first       (ratio->semitones        (fq->ratio (list actual-fq wanted-fq)))) 100.0)));(get-xp 440.0 880.0)
